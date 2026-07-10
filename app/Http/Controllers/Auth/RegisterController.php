e9faa5220df08de2e746622e10e5aed3d286eb2a
dimasneto7 <dimas_fcn@outlook.com>
2026-07-10 19:29:29 -0300

    create user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
@@ -11,5 +13,22 @@ class RegisterController extends Controller
     public function create()
     {
         return Inertia::render('Auth/Register');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|lowercase|email|max:255',
+            'password' => 'required|confirmed|min:3'
+        ]);
+
+        $user = User::create($credentials);
+
+        // send verifcation email
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,32 +1,63 @@
 <script setup>
+import { useForm } from "@inertiajs/vue3";
 import Container from "../../Components/Container.vue";
 import Title from "../../Components/Title.vue";
 import TextLink from "../../Components/TextLink.vue";
 import InputField from "../../Components/InputField.vue";
 import PrimaryBtn from "../../Components/PrimaryBtn.vue";
+import ErrorMessages from "../../Components/ErrorMessages.vue";
+
+const form = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+});
+
+const submit = () => {
+    form.post(route("register"), {
+        onFinish: () => form.reset("password", "password_confirmation"),
+    });
+};
 </script>
 
 <template>
     <Container class="w-1/2">
         <div class="mb-8 text-center">
-            <Title>Register a new account</Title>
+            <Title>Crie sua conta</Title>
             <p>
-                Already have an account?
-                <TextLink routeName="home" label="Login" />''
+                Já tem uma conta?
+                <TextLink routeName="home" label="Login" />
             </p>
         </div>
-        <form class="space-y-6">
-            <InputField label="Name" icon="id-badge" />
-            <InputField label="Email" type="email" icon="at" />
-            <InputField label="Password" type="password" icon="key" />
-            <InputField label="Confirm Password" type="password" icon="key" />
+
+        <!-- Error Messages -->
+        <ErrorMessages :errors="form.errors" />
+
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField label="Name" icon="id-badge" v-model="form.name" />
+            <InputField label="Email" icon="at" v-model="form.email" />
+            <InputField
+                label="Password"
+                type="password"
+                icon="key"
+                v-model="form.password"
+            />
+            <InputField
+                label="Confirm Password"
+                type="password"
+                icon="key"
+                v-model="form.password_confirmation"
+            />
 
             <p class="text-slate-500 text-sm dark:text-slate-400">
-                By registering, you agree to the Terms of Service and Privacy
-                Policy.
+                Fazendo o seu cadastro você concorda com os Termos de Serviço e
+                Políticas de Privacidade.
             </p>
 
-            <PrimaryBtn>Register</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing"
+                >Finalizar Cadastro</PrimaryBtn
+            >
         </form>
     </Container>
 </template>
